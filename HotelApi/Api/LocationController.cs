33051203e6996862c2434ebc7c3421b93169e3a9
herideils1300 <fran.urban.liker.ets@gmail.com>
2026-07-02 15:01:45 +0200

    Removing of the user due to use case uncertainty; Creating of a location controller for location parsing; Adding of a hotel controller with hotel querying;

---
+++ HotelApi/Api/LocationController.cs
@@ -0,0 +1,19 @@
+﻿using HotelApi.Domain.Data.Loaction.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelApi.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+
+        [HttpPost]
+        public ActionResult<HotelQuery> PostLocation(LocationDto location)
+        {
+
+            return Ok();
+        }
+    }
+}
--- HotelApi/Program.cs
+++ HotelApi/Program.cs
@@ -1,6 +1,9 @@
 
 
 
+using HotelApi.Application;
+using HotelApi.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +12,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication().AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
